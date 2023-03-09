a6039d94d2b6046a6f1bb87c3092dfaa3fca32ef
Tong Tang <pichip22@hotmail.com>
2023-03-09 13:33:25 -0500

    add 8 functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(String) {
+    cats.push(String);
+}
+function destructivelyPrependCat(String) {
+    cats.unshift(String);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(String){
+    const newCats = [...cats];
+    newCats.push(String);
+    return newCats;
+}
+function prependCat(String){
+    const newCats = [...cats];
+    newCats.unshift(String);
+    return newCats;
+}
+function removeLastCat(){
+    const newCats = cats.slice(0,-1);
+    return newCats;
+}
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
